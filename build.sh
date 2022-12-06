52b93378021adeee41fec358b4209aadbf532d83
Brennan Seymour <brenno263@gmail.com>
2022-12-06 14:58:42 -0600

    fix gitignore

--- build.sh
+++ build.sh
@@ -7,9 +7,12 @@ clang -c -O0 -Xclang -disable-O0-optnone -g -emit-llvm -S Prog.c -o ProgV3.bc
 cd ../..
 
 
-rm -rf ./ninja
+rm -rf ./ninja/
+mkdir ninja
 cmake -B ninja -G Ninja .
 cd ninja
 ninja
 # cat ../TestPrograms/Correct/Prog.c
 ./Hydrogen.out ../TestPrograms/Buggy/ProgV1.bc ../TestPrograms/Buggy2/ProgV2.bc ../TestPrograms/Correct/ProgV3.bc :: ../TestPrograms/Buggy/Prog.c :: ../TestPrograms/Buggy2/Prog.c :: ../TestPrograms/Correct/Prog.c
+cd ..
+chmod -R 777 ninja
