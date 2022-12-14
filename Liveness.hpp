9d123330960459d73c516a741458ddc9a0bcf251
Brennan Seymour <brenno263@gmail.com>
2022-12-13 20:03:56 -0600

    move liveness functionality into a separate file

---
+++ Liveness.hpp
@@ -0,0 +1,20 @@
+/**
+ * @authors Benjamin Goodall, Brennan Seymour, Marios Tsekitsidis, Garrett Westenskow
+*/
+#ifndef LIVENESS_H
+#define LIVENESS_H
+
+#include "llvm/IR/CFG.h"
+namespace hydrogen_framework {
+
+/* Forward declaration */
+class Graph;
+class Module;
+
+void find_dead_code(Graph *g);
+
+void livenessAnalysis(Module *mod);
+
+}
+
+#endif
